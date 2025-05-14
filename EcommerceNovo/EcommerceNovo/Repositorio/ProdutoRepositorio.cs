8e5bb59401137a009924e99df9c02fb2b2d66cf1
Bruno <pessoal.conta552@gmail.com>
2025-05-14 19:45:35 -0300

    Configurando o banco de dados

---
+++ EcommerceNovo/EcommerceNovo/Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace EcommerceNovo.Repositorio
+{
+    public class ProdutoRepositorio
+    {
+    }
+}
---
+++ script.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE EcommerceNovo;
+USE EcommerceNovo;
+
+CREATE TABLE Usuario(
+	Id int primary key auto_increment, 
+    Nome varchar(50) not null,
+    Email varchar(50) not null,
+    Senha varchar (20) not null
+);
+
+CREATE TABLE Produto(
+	Id int primary key auto_increment,
+    Nome varchar(50) not null,
+    Descricao varchar(200) not null,
+    Quantidade int not null,
+    Preco decimal (12,2)
+);
